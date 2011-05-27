6f14a3edbd272c702e49292269581e5446a1ae95
lucapette <lucapette@gmail.com>
2011-05-27 11:51:24 +0200

    Setting up a new rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarrierwaveNestedForm::Application.config.secret_token = 'bce735f827bf682f5993db10750c624183d7be2e3ac61181dd1ab7d7fd2c0a68f5567ee32df1af36bcf20cf22aa4b2e1e0e3f303a1017580b84d6b3017754d23'
