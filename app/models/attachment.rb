5b1688eca8f92772878926d94c3fda53841bb2df
lucapette <lucapette@gmail.com>
2011-05-27 15:59:52 +0200

    uploader added

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -2,6 +2,8 @@ class Attachment < ActiveRecord::Base
   attr_accessible :description, :file
 
   belongs_to :attachable, :polymorphic => true
+
+  mount_uploader :file, FileUploader
 end
 
 # == Schema Information
